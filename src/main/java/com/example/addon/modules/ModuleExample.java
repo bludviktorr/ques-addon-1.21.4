99e938ab6013957e4c1cf6d52cf2d0e1901cd6b9
bludviktorr <viktorhanak53@icloud.com>
2026-02-12 16:47:57 +0100

    Update ModuleExample.java

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -14,7 +14,7 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 
-public class ModuleExample extends Module {
+public class Ques extends Module{
     private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
     private final SettingGroup sgRender = this.settings.createGroup("Render");
 
@@ -42,8 +42,8 @@ public class ModuleExample extends Module {
     /**
      * The {@code name} parameter should be in kebab-case.
      */
-    public ModuleExample() {
-        super(AddonTemplate.CATEGORY, "world-origin", "An example module that highlights the center of the world.");
+    public Ques() {
+        super(Ques.CATEGORY, "world-origin", "An example module that highlights the center of the world.");
     }
 
     /**
